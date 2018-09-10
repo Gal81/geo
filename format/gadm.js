1b75df57cff3dce54a198d722956672e6d5a05e6
Valerii Galiuta <vg@audienceproject.com>
2018-09-10 18:06:21 +0300

    Add regions mapping

--- format/gadm.js
+++ format/gadm.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 const colors = require('colors');
 
-const getShortCoordinates = (coordinates, type, precision = 1) => {
-  const divider = precision * 1000;
-
+const getShortCoordinates = (coordinates, type, zoom = 1000) => {
   const getShortPair = pair => [
-    Math.round(pair[0] * divider) / divider,
-    Math.round(pair[1] * divider) / divider
+    Math.round(pair[0] * zoom),
+    Math.round(pair[1] * zoom)
   ];
 
   if (type === 'Polygon') {
@@ -19,86 +17,95 @@ const getShortCoordinates = (coordinates, type, precision = 1) => {
   }
 }
 
-const saveRegions = (regions, country) => {
-  let number = 0;
-  let dir = '';
-  let maps = '';
-  let countryCode = '';
-  const regionsList = {};
+const saveRegions = (regions, country, countryCode) => {
+  if (!countryCode) {
+    const error = new Error(` Missing Country Code in! `);
+    return console.error(` ${error.message} `.bgRed.white);
+  }
 
-  Object.keys(regions).forEach(key => {
-    const region = regions[key];
-    const geoJson = {
-      title: key,
-      type: 'FeatureCollection',
-      features: region,
+  const regionsList = {};
+  const dir = `./maps/${countryCode}`;;
+  let number = 0;
+  let maps = '';
+  let mapping = '';
+
+  try {
+    fs.readFile(`${dir}/__mapping.json`, 'utf8', (err, data) => {
+      if (data) {
+        mapping = JSON.parse(data);
+      }
+    });
+  } catch(ex) {
+    console.error(` ${ex.message} `.bgRed.white);
+  }
+
+  const doSave = () => {
+    Object.keys(regions).forEach(key => {
+      const region = regions[key];
+      const geoJson = {
+        title: key,
+        type: 'FeatureCollection',
+        features: region,
+      };
+
+      const regionCode = region[0].id.split('.')[1].toLowerCase();
+      const featureKey = `${countryCode}-${mapping ? mapping[key] : regionCode}-all`;
+      const mapKey = `countries/${countryCode}/${featureKey}`;
+      const map = `Highcharts.maps['${mapKey}'] = ${JSON.stringify(geoJson, null, 2)};\n`;
+
+      maps = `${maps}${map}`;
+      regionsList[key] = regionCode;
+      number++;
+
+      console.log(` ${country} region ‘${key}’ added... `.bgGreen.white);
+    });
+
+
+    const getIndex = list => {
+      const index = {};
+      for (key in list) {
+        index[`${key}, admin2`] = `countries/${countryCode}/${countryCode}-${mapping ? mapping[key] : list[key]}-all.js`;
+      }
+      return index;
+    }
+
+    const extras = {
+      country,
+      regions: regionsList,
+      index: getIndex(regionsList),
+      script: `<script src='maps/${countryCode}/${countryCode}-admin2-all.js'></script>`,
     };
 
-    if (!countryCode) {
+
+
+    if (!fs.existsSync(dir)) {
       try {
-        countryCode = region[0].id.split('.')[0].slice(0, 2).toLowerCase();
+        fs.mkdirSync(dir);
       } catch(ex) {
-        const error = new Error(` Missing Country Code in ‘${key}’ region! `);
-        return console.error(` ${error.message} `.bgRed.white);
+        return console.error(` ${ex.message} ‘${dir}’ `.bgRed.white);
       }
     }
 
-    if (!dir) {
-      dir = `./maps/${countryCode}`;
-    }
+    fs.writeFile(`${dir}/${countryCode}-admin2-all.js`, maps, err => {
+      if(err) {
+        return console.error(` ${err} `.bgRed.white);
+      }
 
-    const regionCode = region[0].id.split('.')[1].toLowerCase();
-    const featureKey = `${countryCode}-${regionCode}-all`;
-    const mapKey = `countries/${countryCode}/${featureKey}`;
-    const map = `Highcharts.maps['${mapKey}'] = ${JSON.stringify(geoJson, null, 2)};\n`;
+      console.log(` ${country} with ${number} regions saved! `.bgGreen.white);
+    });
 
-    maps = `${maps}${map}`;
-    regionsList[key] = regionCode;
-    number++;
+    fs.writeFile(`./maps/${countryCode}/__extras.json`, JSON.stringify(extras, null, 2), err => {
+      if(err) {
+        return console.error(` ${err} `.bgRed.white);
+      }
 
-    console.log(` ${country} region ‘${key}’ added... `.bgGreen.white);
-  });
-
-  const getIndex = list => {
-    const index = {};
-    for (key in list) {
-      index[`${key}, admin2`] = `countries/${countryCode}/${countryCode}-${list[key]}-all.js`;
-    }
-    return index;
+      console.log(` ${country}’s extras saved to ‘./maps/${countryCode}/__extras.json’ `.bgMagenta.white);
+    });
   }
 
-  const extras = {
-    country,
-    regions: regionsList,
-    index: getIndex(regionsList),
-    script: `<script src='maps/${countryCode}/${countryCode}-admin2-all.js'></script>`,
-  };
-
-
-
-  if (!fs.existsSync(dir)) {
-    try {
-      fs.mkdirSync(dir);
-    } catch(ex) {
-      return console.error(` ${ex.message} ‘${dir}’ `.bgRed.white);
-    }
-  }
-
-  fs.writeFile(`${dir}/${countryCode}-admin2-all.js`, maps, err => {
-    if(err) {
-      return console.error(` ${err} `.bgRed.white);
-    }
-
-    console.log(` ${country} with ${number} regions saved! `.bgGreen.white);
-  });
-
-  fs.writeFile(`./maps/${countryCode}/__extras.json`, JSON.stringify(extras, null, 2), err => {
-    if(err) {
-      return console.error(` ${err} `.bgRed.white);
-    }
-
-    console.log(` ${country}’s extras saved to ‘./maps/${countryCode}/__extras.json’ `.bgMagenta.white);
-  });
+  setTimeout(() => {
+    doSave();
+  }, 1000);
 }
 
 
@@ -110,6 +117,7 @@ fs.readFile(`./tmp/${fileName}.json`, 'utf8', (error, geoJson) => {
   }
 
   let country = '';
+  let countryCode = '';
   const regions = {};
   const { features } = JSON.parse(geoJson);
   features.forEach(feature => {
@@ -128,6 +136,7 @@ fs.readFile(`./tmp/${fileName}.json`, 'utf8', (error, geoJson) => {
     }
 
     const regionName = properties['NAME_1'];
+    const splitted = featureID.split('.');
 
     if (!regions[regionName]) {
       regions[regionName] = [];
@@ -137,7 +146,10 @@ fs.readFile(`./tmp/${fileName}.json`, 'utf8', (error, geoJson) => {
       country = properties['NAME_0'];
     }
 
-    const splitted = featureID.split('.');
+    if (!countryCode) {
+      countryCode = splitted[0].slice(0, 2);
+    }
+
     const hcKey = splitted.join('-').toLowerCase();
     const hcA2 = featureID.split('.')[splitted.lenght - 1];
     regions[regionName].push({
@@ -157,5 +169,5 @@ fs.readFile(`./tmp/${fileName}.json`, 'utf8', (error, geoJson) => {
     });
   });
 
-  saveRegions(regions, country);
+  saveRegions(regions, country, countryCode);
 });
