70f4fc5099889526449d1e74d25ffb8d5a7894a6
Valerii Galiuta <vg@audienceproject.com>
2018-09-25 10:36:14 +0300

    Add list of countries

---
+++ format/dump/countries.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const colors = require('colors');
+
+// input: './maps/tmp.json'
+// output: './maps/output.json'
+fs.readFile('./maps/tmp.json', 'utf8', (error, tmp) => {
+  if(error) {
+    return console.error(` ${error} `.bgRed.white);
+  }
+
+  const origin = JSON.parse(tmp);
+  const file = {
+    countries: Object
+      .keys(origin)
+      .sort()
+      .map(key => ({
+        name: origin[key],
+        isoA2: key,
+        isoA3: '',
+      }))
+    };
+
+  fs.writeFile('./maps/output.json', JSON.stringify(file, null, 2), errr => {
+    if(errr) {
+      return console.error(` ${errr} `.bgRed.white);
+    }
+
+    console.log(` DONE `.bgBlue.white);
+  });
+});
